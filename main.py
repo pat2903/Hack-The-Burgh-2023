fd2ed5d54c5d770791bdf9c489be5b408147c8e6
Al1ceG <94125456+Al1ceG@users.noreply.github.com>
2023-03-04 19:13:34 +0000

    panda vs not

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from os import listdir
 from shutil import copyfile
 from random import seed
 from random import random
+import numpy
 
 # baseline model for the dogs vs cats dataset
 import sys
@@ -40,7 +41,7 @@ from keras.preprocessing.image import ImageDataGenerator
 # # define ratio of pictures to use for validation
 # val_ratio = 0.25
 #
-# # copy training dataset images into subdirectories
+# copy training dataset images into subdirectories
 # src_directory = 'train/'
 # for file in listdir(src_directory):
 #     src = src_directory + '/' + file
@@ -60,7 +61,7 @@ from keras.preprocessing.image import ImageDataGenerator
 # # enumerate files in the directory
 # for file in listdir(folder):
 #     # determine class
-#     output = 0.0
+#     output = 1.0
 #     if file.startswith('Panda'):
 #         output = 1.0
 #     # load image
@@ -82,57 +83,98 @@ from keras.preprocessing.image import ImageDataGenerator
 
 
 # define cnn model
-def define_model():
-    model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-    model.add(Dense(1, activation='sigmoid'))
-    # compile model
-    opt = SGD(lr=0.001, momentum=0.9)
-    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
-    return model
+# def define_model():
+#     model = Sequential()
+#     model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
+#     model.add(MaxPooling2D((2, 2)))
+#     model.add(Flatten())
+#     model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+#     model.add(Dense(1, activation='sigmoid'))
+#     # compile model
+#     opt = SGD(lr=0.001, momentum=0.9)
+#     model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+#     return model
+#
+#
+# # plot diagnostic learning curves
+# def summarize_diagnostics(history):
+#     # plot loss
+#     pyplot.subplot(211)
+#     pyplot.title('Cross Entropy Loss')
+#     pyplot.plot(history.history['loss'], color='blue', label='train')
+#     pyplot.plot(history.history['val_loss'], color='orange', label='test')
+#     # plot accuracy
+#     pyplot.subplot(212)
+#     pyplot.title('Classification Accuracy')
+#     pyplot.plot(history.history['accuracy'], color='blue', label='train')
+#     pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
+#     # save plot to file
+#     filename = sys.argv[0].split('/')[-1]
+#     pyplot.savefig(filename + '_plot.png')
+#     pyplot.close()
+#
+#
+# # run the test harness for evaluating a model
+# def run_test_harness():
+#     # define model
+#     model = define_model()
+#     # create data generator
+#     datagen = ImageDataGenerator(rescale=1.0 / 255.0)
+#     # prepare iterators
+#     train_it = datagen.flow_from_directory('train/',
+#                                            class_mode='binary', batch_size=64, target_size=(200, 200))
+#     test_it = datagen.flow_from_directory('test/',
+#                                           class_mode='binary', batch_size=64, target_size=(200, 200))
+#     # fit model
+#     history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
+#                                   validation_data=test_it, validation_steps=len(test_it), epochs=20, verbose=0)
+#     # evaluate model
+#     _, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=0)
+#     print('> %.3f' % (acc * 100.0))
+#     # learning curves
+#     summarize_diagnostics(history)
+#     model.save('final_model.h5')
+#
+#
+# # entry point, run the test harness
+# run_test_harness()
 
 
-# plot diagnostic learning curves
-def summarize_diagnostics(history):
-    # plot loss
-    pyplot.subplot(211)
-    pyplot.title('Cross Entropy Loss')
-    pyplot.plot(history.history['loss'], color='blue', label='train')
-    pyplot.plot(history.history['val_loss'], color='orange', label='test')
-    # plot accuracy
-    pyplot.subplot(212)
-    pyplot.title('Classification Accuracy')
-    pyplot.plot(history.history['accuracy'], color='blue', label='train')
-    pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
-    # save plot to file
-    filename = sys.argv[0].split('/')[-1]
-    pyplot.savefig(filename + '_plot.png')
-    pyplot.close()
+# make a prediction for a new image.
+from keras.utils import load_img
+from keras.utils import img_to_array
+from keras.models import load_model
 
+# load and prepare the image
+def load_image(filename):
+	# load the image
+	img = load_img(filename, target_size=(200, 200))
+	# convert to array
+	img = img_to_array(img)
+	# reshape into a single sample with 3 channels
+	img = img.reshape(1, 200, 200, 3)
+	# center pixel data
+	img = img.astype('float32')
+	img = img - [123.68, 116.779, 103.939]
+	return img
 
-# run the test harness for evaluating a model
-def run_test_harness():
-    # define model
-    model = define_model()
-    # create data generator
-    datagen = ImageDataGenerator(rescale=1.0 / 255.0)
-    # prepare iterators
-    train_it = datagen.flow_from_directory('dataset/',
-                                           class_mode='binary', batch_size=64, target_size=(200, 200))
-    test_it = datagen.flow_from_directory('dataset/',
-                                          class_mode='binary', batch_size=64, target_size=(200, 200))
-    # fit model
-    history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
-                                  validation_data=test_it, validation_steps=len(test_it), epochs=20, verbose=0)
-    # evaluate model
-    _, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=0)
-    print('> %.3f' % (acc * 100.0))
-    # learning curves
-    summarize_diagnostics(history)
+# load an image and predict the class
+def run_example():
+	# define location of dataset
+	folder = 'massTesting/'
+	count = 0
+	# enumerate files in the directory
+	for file in listdir(folder):
+		if file == '.DS_Store':
+			continue
+		# load the image
+		img = load_image(folder + file)
+		# load model
+		model = load_model('final_model.h5')
+		# predict the class
+		count += numpy.round(model.predict(img), 0)
+		# print percentage of pandas
+	print(count/len(listdir(folder)))
 
-
-# entry point, run the test harness
-run_test_harness()
+# entry point, run the example
+run_example()
